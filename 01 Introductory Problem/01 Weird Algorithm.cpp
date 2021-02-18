135e4d23fe3924e1ad2139b63b6e3d3237ce3188
Shoichi <78679833+lShoichil@users.noreply.github.com>
2021-02-18 17:17:55 +1100

    Create 01 Weird Algorithm.cpp

---
+++ 01 Introductory Problem/01 Weird Algorithm.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+ 
+using namespace std;
+ 
+int main() {
+    //freopen("OUTPUT.out","w",stdout);
+    //freopen("INPUT.out","r",stdout);
+ 
+    ll n; cin >> n;
+
+    cout << n << " ";
+   while (n != 1){
+       if (n % 2 == 0){
+           n /= 2;
+           cout << n << " ";
+       } else {
+           n = (n * 3) + 1;
+           cout << n << " ";
+       }
+   }
+ 
+    return 0;
+}
